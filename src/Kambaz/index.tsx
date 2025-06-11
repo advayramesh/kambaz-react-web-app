bc268f419b1ad08c2d84f46eb1a0e2dfe851d0cb
advayramesh <advay.ramesh@gmail.com>
2025-06-10 20:55:45 -0700

    fixed a5 issues

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -8,41 +8,10 @@ import Labs from "../Labs/index";
 import "./styles.css";
 import ProtectedRoute from "./Account/ProtectedRoute";
 import Session from "./Account/Session";
-import * as userClient from "./Account/client";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import * as courseClient from "./Courses/client";
 
 
 export default function Kambaz() {
-  const [courses, setCourses] = useState<any[]>([]);
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const fetchCourses = async () => {
-    try {
-      const courses = await userClient.findMyCourses();
-      setCourses(courses);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  const addNewCourse = async (course: any) => {
-    try {
-      const newCourse = await userClient.createCourse(course);
-      setCourses([...courses, newCourse]);
-    } catch (error) {
-      console.error("Failed to add course", error);
-    }
-  };
-  const updateCourse = async (course: any) => {
-    await courseClient.updateCourse(course);
-    setCourses(courses.map((c) => 
-      c._id === course._id ? course : c
-    ));
-  };
-  useEffect(() => {
-    fetchCourses();
-  }, [currentUser]);
-
+  
 
   return (
     <Session>
@@ -52,7 +21,7 @@ export default function Kambaz() {
         <Routes>
           <Route path="/" element={<Navigate to="Account" />} />
           <Route path="/Account/*" element={<Account />} />
-          <Route path="/Dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/Dashboard" element={<ProtectedRoute><Dashboard  /></ProtectedRoute>} />
           <Route path="/Courses/:cid/*" element={<Courses />} />
           <Route path="/Calendar" element={<h1>Calendar</h1>} />
           <Route path="/Inbox" element={<h1>Inbox</h1>} />
