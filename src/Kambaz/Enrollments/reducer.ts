399312cc6c3c92254d403d5a23714ac96eabee63
advayramesh <advay.ramesh@gmail.com>
2025-06-10 20:43:15 -0700

    fixed a5

--- src/Kambaz/Enrollments/reducer.ts
+++ src/Kambaz/Enrollments/reducer.ts
@@ -2,8 +2,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
+export type Enrollment = {
+  _id: string;
+  user: string;
+  course: string;
+};
+
 const initialState = {
-  enrollments: []
+  enrollments: [] as Enrollment[]
 };
 
 const enrollmentsSlice = createSlice({
