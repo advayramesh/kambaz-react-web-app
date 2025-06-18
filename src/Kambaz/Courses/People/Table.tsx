8b4d780d3b5f6b2aa0ce134c4eee90126ae556af
advayramesh <advay.ramesh@gmail.com>
2025-06-17 22:29:31 -0700

    fixed crash

--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -12,7 +12,10 @@
       <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
       </thead>
       <tbody>
-      {users.map((user: any) => (
+      {users.map((user: any) => {
+        if (!user || !user._id) return null;
+        return (
+        
         <tr key={user._id}>
           <td className="wd-full-name text-nowrap">
           <Link to={`/Kambaz/Account/Users/${user._id}`} className="text-decoration-none">
@@ -27,7 +30,7 @@
           <td className="wd-last-activity">{user.lastActivity}</td>
           <td className="wd-total-activity">{user.totalActivity}</td>
         </tr>
-      ))}
+      );})}
 
       </tbody>
     </Table>
